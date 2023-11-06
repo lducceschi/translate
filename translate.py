a58865e8c1a0fa8be1bda6fb877a17a0ae329e3b
lducceschi <147488509+lducceschi@users.noreply.github.com>
2023-11-06 12:32:02 +0100

    Added main scriprt

---
+++ translate.py
@@ -0,0 +1,15 @@
+from googletrans import Translator
+
+def translate_to_italian(text):
+    translator = Translator()
+
+    try:
+        translated = translator.translate(text, src='en', dest='it')
+        return translated.text
+    except Exception as e:
+        return str(e)
+
+if __name__ == "__main__":
+    text_to_translate = input("Enter the text in English to translate to Italian: ")
+    translated_text = translate_to_italian(text_to_translate)
+    print("Translated to Italian: ", translated_text)
